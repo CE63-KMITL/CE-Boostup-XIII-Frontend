2f2d19c32be61523edafabfd145cf3d4bcc30f90
AzPepoze <65204787+AzPepoze@users.noreply.github.com>
2025-01-16 20:51:41 +0700

    implement dark mode support and add button component; update styles and remove unused files

---
+++ src/components/Button.svelte
@@ -0,0 +1,12 @@
+<button>
+	<slot></slot>
+</button>
+
+<style lang="scss">
+	button {
+		border: 1px solid var(--color-outline);
+		background: var(--color-button);
+		border-radius: 10px;
+		padding: 5px;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
-	import '../app.css';
+	import "../app.css";
 	let { children } = $props();
+
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		const URL_Params = new URLSearchParams(window.location.search);
+
+		if (URL_Params.get("dark")) {
+			localStorage.setItem("dark", URL_Params.get("dark"));
+		}
+
+		if (localStorage.getItem("dark") === "true") {
+			document.documentElement.setAttribute("dark", "");
+		} else {
+			document.documentElement.removeAttribute("dark");
+		}
+	});
 </script>
 
 {@render children()}
