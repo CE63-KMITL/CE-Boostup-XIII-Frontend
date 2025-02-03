b991b70e48f5f0bbc5a1116eba588e271ab6a1c9
khom19 <khom2548@gmail.com>
2025-02-03 23:23:12 +0700

    Full Problem/layout (without responsive)

--- src/routes/Problem/+page.svelte
+++ src/routes/Problem/+page.svelte
@@ -3,14 +3,44 @@
 </script>
 
 <div class="ProblemContainer">
-    <textarea class="CodeInput"></textarea>
+    <div class="CodeInputBox">
+        <div class="WrapButton">
+            <button class="Run">Run</button>
+            <button class="Submit">Submit</button>
+        </div>
+        <textarea class="CodeInput" placeholder="Enter your code..."></textarea>
+    </div>
     <div class="InfoAndTestcaseBox">
         <div class="HeadAndDescriptionBox">
             <h1 class="ProblemHead">Head</h1>
             <p class="ProblemDescription">Description</p>
         </div>
         <div class="TestcaseBox">
-
+            <div class="TestcaseWrap">
+                <p class="TestcaseNo">Test case1</p>
+                <p class="InputAndOutput">Input: </p>
+                <p class="InputAndOutput">Output: </p>
+            </div>
+            <div class="TestcaseWrap">
+                <p class="TestcaseNo">Test case1</p>
+                <p class="InputAndOutput">Input: </p>
+                <p class="InputAndOutput">Output: </p>
+            </div>
+            <div class="TestcaseWrap">
+                <p class="TestcaseNo">Test case1</p>
+                <p class="InputAndOutput">Input: </p>
+                <p class="InputAndOutput">Output: </p>
+            </div>
+            <div class="TestcaseWrap">
+                <p class="TestcaseNo">Test case1</p>
+                <p class="InputAndOutput">Input: </p>
+                <p class="InputAndOutput">Output: </p>
+            </div>
+            <div class="TestcaseWrap">
+                <p class="TestcaseNo">Test case1</p>
+                <p class="InputAndOutput">Input: </p>
+                <p class="InputAndOutput">Output: </p>
+            </div>
         </div>
     </div>
 </div>
@@ -24,12 +54,49 @@
         height: 100%;
         background-color: wheat;
     }
-    .CodeInput{
-        width: 70%;
+    .CodeInputBox{
         height: 80%;
+        width: 70%;
+    }
+    .WrapButton{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 7.5%;
+        background-color: burlywood;
+        border: 2px solid saddlebrown;
+        border-top-left-radius: 20px;
+        border-top-right-radius: 20px;
+        gap: 20px;
+    }
+    .Run{
+        width: 12.5vh;
+        background-color: peachpuff;
+        border: 1px solid saddlebrown;
+        border-radius: 10px;
+        padding: 5px;
+        font-size: 2vh;
+        font-weight: 600;
+    }
+    .Submit{
+        width: 12.5vh;
+        background-color: peachpuff;
+        border: 1px solid saddlebrown;
+        border-radius: 10px;
+        padding: 5px;
+        font-size: 2vh;
+        font-weight: 600;
+    }
+    .CodeInput{
+        width: 100%;
+        height: 92.5%;
         background-color: antiquewhite;
         border: 2px solid saddlebrown;
+        border-top: none;
         border-radius: 20px;
+        border-top-left-radius: 0px;
+        border-top-right-radius: 0px;
         resize: none;
         padding: 15px;
         padding-left: 20px;
@@ -38,12 +105,15 @@
     }
     .CodeInput:focus{
         border: 2px solid saddlebrown;
-        outline: 1px auto saddlebrown;
+        border-top: none;
+        outline: none;
         box-shadow: none;
     }
     .InfoAndTestcaseBox{
         display: flex;
-        justify-content: center;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
         padding-top: 20px;
         width: 25%;
         height: 80%;
@@ -53,7 +123,7 @@
     }
     .HeadAndDescriptionBox{
         width: 90%;
-        height: 45%;
+        height: 30%;
         background-color: antiquewhite;
         border: 2px solid saddlebrown;
         border-radius: 10px;
@@ -79,5 +149,34 @@
         white-space: normal;
         word-break: break-word;
         overflow: hidden;
+    }
+    .TestcaseBox{
+        display: flex;
+        flex-direction: column;
+        width: 90%;
+        height: 60%;
+        background-color: antiquewhite;
+        border: 2px solid saddlebrown;
+        border-radius: 10px;
+        padding: 15px;
+        gap: 30px;
+        font-size: 2vh;
+        white-space: normal;
+        word-break: break-word;
+        overflow: auto;
+    }
+    .TestcaseBox::-webkit-scrollbar{
+        width: 10px;
+    }
+    .TestcaseBox::-webkit-scrollbar-thumb {
+        background: saddlebrown;
+        border-radius: 10px;
+    }
+    .TestcaseBox::-webkit-scrollbar-thumb:hover {
+        background: #8b5a2b;
+    }
+    .TestcaseNo{
+        font-size: 2.25vh;
+        font-weight: 600;
     }
 </style>
