a575211a1398528c6f783d89203dae082c306775
khom19 <khom2548@gmail.com>
2025-02-16 17:05:19 +0700

    full Problem create layout

--- src/routes/Problem/Create/+page.svelte
+++ src/routes/Problem/Create/+page.svelte
@@ -5,7 +5,6 @@
     let user = { 
         name: "John Doe"
     }
-    let count = 1
     let options = [
         { id: "4", value: "K" },
         { id: "3", value: "H" },
@@ -13,10 +12,14 @@
         { id: "1", value: "M" },
     ];
     let first_option = ''
+    let test_cases = [{ input: '', output: '', hidden: false }];
 
-    //repeat add TestCase
-    function* repeat(start = 0, end = count) {
-        for (let i = start; i < end; i++) yield i;
+    //Add TestCase and Delete
+    function addTestCase() {
+        test_cases = [...test_cases, { input: '', output: '', hidden: false }];
+    }
+    function deleteTestCase(index: number) {
+        test_cases = test_cases.filter((_, i) => i !== index);
     }
 </script>
 
@@ -30,7 +33,7 @@
         <div class="UserAndExDetails">
             <div class="Username">
                 <h1 class="Avatar" style="color: green; font-weight:600">//Add avatar icon</h1>
-                <p class="HeadText">{user.name}</p>
+                <h1 class="HeadText">{user.name}</h1>
             </div>
             <div class="TagsBox">
                 <h1 class="HeadText">Tags</h1>
@@ -58,12 +61,23 @@
         </div>
     </div>
     <div class="TestCase">
-        <div class="TestcaseBox">
-            <button class="RunAll">Run All</button>
-            {#each repeat() as i}
-                <div>TestCase</div>
+        <button class="RunAll">Run All</button>
+        <div class="TestCaseBox">
+            {#each test_cases as test_case , i}
+                <div class="TestCaseContainer">
+                    <div class="TestCaseCover">
+                        <h1 class="HeadText">Test Case {i+1}</h1>
+                        <div class="HiddenTestcaseBox">
+                            <input class="CheckBox" type="checkbox" bind:checked={test_case.hidden}>
+                            <p>Hidden Test Case</p>
+                        </div>
+                        <textarea class="TestCaseInput" placeholder="Input here"  bind:value={test_case.input}></textarea>
+                        <textarea  class="TestCaseOutput" placeholder="Output here" bind:value={test_case.output}></textarea>
+                    </div>
+                    <button class="Delete" on:click={() => deleteTestCase(i)}>Delete</button>
+                </div>
             {/each}
-            <button class="AddTestCase" on:click={() => count++}>Add</button>
+            <button class="AddTestCase" on:click={() => addTestCase()}>+</button>
         </div>
     </div>
 </div>
@@ -116,10 +130,11 @@
         font-size: 1.25rem;
         line-height: 1.75rem;
         font-weight: 600;
+        align-self: self-start;
     }
     .Tags{
         border: 1px solid var(--outline);
-		border-radius: 10px;
+		border-radius: 5px;
 		padding: 5px;
     }
     .ProblemNameInput{
@@ -169,7 +184,7 @@
         align-items: center;
         justify-content: center;
         gap: 2.5%;
-        margin-top: 7rem;
+        margin-top: 9rem;
     }
     .CodeInputBox{
         width: 55%;
@@ -224,9 +239,6 @@
         justify-content: center;
         gap: 12px;
     }
-    .HeadText{
-        align-self: self-start;
-    }
     .Output{
         height: 40rem;
         width: 100%;
@@ -240,26 +252,129 @@
         overflow: auto;
     }
     .TestCase{
-        width: 100%;
+        max-width: 90%;
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
         gap: 16px;
-        margin-top: 7rem;
+        margin-top: 9rem;
     }
     .TestCaseBox{
-        width: 90%;
         display: flex;
-        flex-direction: row;
         align-items: center;
-        justify-content: center;
+        gap: 16px;
+        margin-bottom: 2rem;
+        overflow-x: auto;        
+        white-space: nowrap;         
+        scroll-behavior: smooth;
+        max-width: 100%;
+        padding-bottom: 15px;
+    }
+    .TestCaseBox::-webkit-scrollbar{
+        width: 1vh;
+        height: 1vh;
+    }
+    .TestCaseBox::-webkit-scrollbar-thumb {
+        background: var(--darker);
+        border-radius: 10px;
+    }
+    .TestCaseBox::-webkit-scrollbar-thumb:hover {
+        background: var(--darker-50);
+    }
+    .TestCaseBox:focus{
+        box-shadow: none;
     }
     .RunAll{
         border: 1px solid var(--outline);
 		background: var(--button-bg);
 		border-radius: 10px;
 		padding: 5px;
+    }
+    .TestCaseContainer{
+        min-width: 25rem;
+        max-width: 25rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 12px;
+    }
+    .TestCaseCover{
+        display: flex;
+        width: 100%;
+        flex-direction: column;
+        gap: 12px;
+    }
+    .HiddenTestcaseBox{
+        display: flex;
+        align-items: center;
+        gap: 6px;
+        color: var(--text);
+        font-size: 1rem;
+        line-height: 1.5rem;
+    }
+    .CheckBox{
+        width: 1.25rem;
+        height: 1.5rem;
+        appearance: none;
+        border: 1px solid var(--outline);
+        border-radius: 5px; 
+        cursor: pointer;
+    }
+    .CheckBox:checked::after{
+        content: '✓';
+        color: var(--text);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: transparent;
+        font-size: 1rem;
+    }
+    .TestCaseInput{
+        height: 25rem;
+        width: 100%;
+        border: 1px solid var(--outline);
+        border-radius: 5px;
+        padding: 5px;
+        font-size: 1rem;
+        line-height: 1.5rem;
+        resize: none;
+    }
+    .TestCaseOutput{
+        height: 15rem;
+        width: 100%;
+        border: 1px solid var(--outline);
+        border-radius: 5px;
+        padding: 5px;
+        font-size: 1rem;
+        line-height: 1.5rem;
+        resize: none;
+    }
+    .AddTestCase{
+        display: flex;      
+        align-items: center;   
+        justify-content: center; 
+        position: relative;
+        top: 14px;
+        min-width: 7.5rem;
+        max-width: 7.5rem;
+        height: 40.7rem;
+        border: 1px solid var(--outline);
+        border-radius: 5px;
+        padding: 5px;
+        font-size: 2.25rem;
+        line-height: 2.75rem;
+        font-weight: 600;
+    }
+    .Delete{
+        border: 1px solid var(--outline);
+		background: var(--button-bg);
+		border-radius: 10px;
+		padding: 5px;
+        color: var(--text);
+        font-size: 1rem;
+        line-height: 1.5rem;
     }
     @media (max-width: 1000px){
         .CodeInputandOutput{
@@ -271,6 +386,14 @@
         }
         .TerminalBox{
             width: 90%;
+        }
+        .TestCaseContainer{
+            min-width: 17.5rem;
+            max-width: 17.5rem;
+        }
+        .AddTestCase{
+            min-width: 3.5rem;
+            max-width: 3.5rem;
         }
     }
 </style>
