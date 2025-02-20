de84a0ad4606f12091197b6f5e0d46f459687ca3
Khom Wanitkittikul <khom2548@gmail.com>
2025-02-20 16:04:03 +0700

    Update Button.svelte

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
-	export let style = {};
+    export let style: Record<string, string> = {};
+    export let className = "";
 
-	let className = "";
-	export { className as class };
+    export { className as class };
 
-	const formatToHTMLStyleFromObject = (styleObj) => {
-        return Object.entries(style).reduce((acc, [key, value]) => `${acc} ${key}: ${value};`, '');
-	};
+    const formatToHTMLStyleFromObject = (styleObj: Record<string, string>) => {
+        return Object.entries(styleObj)
+            .map(([key, value]) => `${key}: ${value};`)
+            .join(' ');
+    };
 
-	$: styleHTML = formatToHTMLStyleFromObject(style);
+    $: styleHTML = formatToHTMLStyleFromObject(style);
 </script>
 
-<button style={styleHTML} class={className}>
-	<slot></slot>
+<button style={styleHTML} class={className} on:click>
+    <slot></slot>
 </button>
 
 <style lang="scss">
-	button {
+    button {
 		border: 1px solid var(--outline);
 		background: var(--button-bg);
 		border-radius: 10px;
