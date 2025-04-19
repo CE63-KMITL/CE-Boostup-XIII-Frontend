3297c6fa837baac44c5468bc58b3b0414065b5c4
AzPepoze <pladumguy@gmail.com>
2025-04-19 22:56:06 +0700

    refactor: enhance API call handling and improve problem detail display

--- src/lib/API.ts
+++ src/lib/API.ts
@@ -1,11 +1,23 @@
 const API_HOST = import.meta.env.VITE_API_HOST;
 
-export function call(route: string, method: string, data: any) {
-	return fetch(`${API_HOST}/${route}`, {
-		method,
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	});
+export async function call(route: string, method: string = "GET", data: any = null) {
+	try {
+		const response = await fetch(`${API_HOST}${route}`, {
+			method,
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: data ? JSON.stringify(data) : null,
+		});
+
+		const contentType = response.headers.get("content-type");
+		if (contentType && contentType.indexOf("application/json") !== -1) {
+			return await response.json();
+		} else {
+			return await response.text();
+		}
+	} catch (error) {
+		alert(`${route}\n\n${error}`);
+		return null;
+	}
 }
