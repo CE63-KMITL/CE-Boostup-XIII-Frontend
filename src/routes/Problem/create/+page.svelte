a3b8cd434919ae0cede143e1ab70294a7799e2b7
khom19 <khom2548@gmail.com>
2025-04-21 12:58:07 +0700

    make problem/create layout from theme

--- src/routes/Problem/create/+page.svelte
+++ src/routes/Problem/create/+page.svelte
@@ -30,7 +30,9 @@
 	}
 
 	function deleteTestCase(index: number) {
-		test_cases = test_cases.filter((_, i) => i !== index);
+		if(test_cases.length != 1){
+			test_cases = test_cases.filter((_, i) => i !== index);
+		}
 	}
 
 	function enableHorizontalScroll(node: HTMLElement) {
@@ -92,103 +94,85 @@
 	function Enable_Node(target_node: HTMLElement) {
 		target_node.removeAttribute("disabled");
 	}
-
-	const done = {
-		position: "fixed",
-		bottom: "20px",
-		right: "20px",
-	};
-
-	const deleteTC = {
-		color: "var(--text)",
-		"font-size": "1rem",
-		"line-height": "1.5rem",
-	};
-
-	const addTC = {
-		display: "flex",
-		"align-items": "center",
-		"justify-content": "center",
-		position: "relative",
-		top: "14px",
-		"min-width": "7.5rem",
-		"max-width": "7.5rem",
-		height: "61.25vh",
-		"font-size": "2.25rem",
-		"line-height": "2.75rem",
-		"font-weight": "600",
-		"border-radius": "5px",
-		"scroll-snap-align": "end",
-	};
 </script>
 
 <div class="ProblemCreateContianer">
 	<div class="Navbar"></div>
 	<div id="MainScroll" class="ScrollSnap" use:enableVerticalScroll>
 		<div class="ProblemInfo">
-			<div class="ProblemHead">
-				<h1 class="HeadText">Exercise Name</h1>
-				<textarea class="ProblemNameInput" placeholder="Exercise Name"></textarea>
-			</div>
-			<div class="UserAndExDetails">
-				<div class="Username">
-					<h1 class="Avatar" style="color: green; font-weight:600">//Add avatar icon</h1>
-					<h1 class="HeadText">{user.name}</h1>
+			<div class="ProblemWrap">
+				<div class="ProblemHead">
+					<h1 class="HeadText">Exercise Name</h1>
+					<textarea class="ProblemNameInput" placeholder="Exercise Name"></textarea>
 				</div>
-				<div class="TagsBox">
-					<h1 class="HeadText">Tags</h1>
-					<select class="Tags" bind:value={first_option}>
-						{#each options as option}
-							<option value={option.id}>{option.value}</option>
-						{/each}
-					</select>
+				<div class="UserAndExDetails">
+					<div class="Username">
+						<h1 class="Avatar" style="color: green; font-weight:600">//Add avatar icon</h1>
+						<h1 class="HeadText">{user.name}</h1>
+					</div>
+					<div class="TagsBox">
+						<h1 class="HeadText">Tags</h1>
+						<select class="Tags" bind:value={first_option}>
+							{#each options as option}
+								<option value={option.id}>{option.value}</option>
+							{/each}
+						</select>
+					</div>
+					<textarea class="ProblemDetailsInput" placeholder="Type exercise's detail here"></textarea>
 				</div>
-				<textarea class="ProblemDetailsInput" placeholder="Type exercise's detail here"></textarea>
 			</div>
-			<Button class="DoneButton" style={done}>Done</Button>
+			<Button class="DoneButton">Done</Button>
 		</div>
 		<div class="CodeInputandOutput">
-			<div class="CodeInputBox">
-				<div class="CodeInputHead">
-					<h1 class="HeadText">Code</h1>
-					<Button class="Run">Run</Button>
+			<div class="CodeInputandOutputWrap">
+				<div class="CodeInputBox">
+					<div class="CodeInputHead">
+						<h1 class="HeadText">Code</h1>
+						<Button class="Run">Run</Button>
+					</div>
+					<textarea class="CodeInput" placeholder="Code here"></textarea>
 				</div>
-				<textarea class="CodeInput" placeholder="Code here"></textarea>
+				<div class="TerminalBox">
+					<h1 class="HeadText">Terminal</h1>
+					<textarea class="Output" placeholder="Output here"></textarea>
+				</div>
 			</div>
-			<div class="TerminalBox">
-				<h1 class="HeadText">Terminal</h1>
-				<textarea class="Output" placeholder="Output here"></textarea>
-			</div>
+			<Button class="DoneButton">Done</Button>
 		</div>
 		<div class="TestCase">
-			<Button class="RunAll">Run All</Button>
-			<div
-				class="TestCaseBox"
-				role="region"
-				use:enableHorizontalScroll
-				on:mouseenter={() => Disable_Node(document.getElementById("MainScroll"))}
-				on:mouseleave={() => Enable_Node(document.getElementById("MainScroll"))}
-			>
-				{#each test_cases as test_case, i}
-					<div class="TestCaseContainer">
-						<div class="TestCaseCover">
-							<h1 class="HeadText">Test Case {i + 1}</h1>
-							<div class="HiddenTestcaseBox">
-								<input class="CheckBox" type="checkbox" bind:checked={test_case.hidden} />
-								<p>Hidden Test Case</p>
+			<div class="TestCaseBoxWrap">
+				<div
+					class="TestCaseBox"
+					role="region"
+					use:enableHorizontalScroll
+					on:mouseenter={() => Disable_Node(document.getElementById("MainScroll"))}
+					on:mouseleave={() => Enable_Node(document.getElementById("MainScroll"))}
+				>
+					{#each test_cases as test_case, i}
+						<div class="TestCaseContainer">
+							<div class="TestCaseCover">
+								<h1 class="HeadText">Test Case {i + 1}</h1>
+								<div class="HiddenTestcaseBox">
+									<input class="CheckBox" type="checkbox" bind:checked={test_case.hidden} />
+									<p>Hidden Test Case</p>
+								</div>
+								<textarea class="TestCaseInput" placeholder="Input here" bind:value={test_case.input}
+								></textarea>
+								<textarea
+									class="TestCaseOutput"
+									placeholder="Output here"
+									bind:value={test_case.output}
+								></textarea>
 							</div>
-							<textarea class="TestCaseInput" placeholder="Input here" bind:value={test_case.input}
-							></textarea>
-							<textarea
-								class="TestCaseOutput"
-								placeholder="Output here"
-								bind:value={test_case.output}
-							></textarea>
+							<Button class="Delete" on:click={() => deleteTestCase(i)}>Delete</Button>
 						</div>
-						<Button class="Delete" style={deleteTC} on:click={() => deleteTestCase(i)}>Delete</Button>
-					</div>
-				{/each}
-				<Button class="AddTestCase" style={addTC} on:click={addTestCase}>+</Button>
+					{/each}
+					<Button class="AddTestCase" on:click={addTestCase}>+</Button>
+				</div>
+			</div>
+			<div class="ButtonWrap">
+				<Button class="RunAll">Run All</Button>
+				<Button class="DoneButton">Done</Button>
 			</div>
 		</div>
 	</div>
@@ -199,7 +183,6 @@
 		width: 100%;
 		max-height: 100vh;
 		height: auto;
-		background-color: var(--bg);
 		display: flex;
 		flex-direction: column;
 		align-items: center;
@@ -228,8 +211,15 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-		gap: 16px;
 		scroll-snap-align: start;
+		gap: 1rem;
+	}
+	.ProblemWrap {
+		width: 90%;
+		background-color: var(--bg-50);
+		border: 1px solid var(--outline);
+		border-radius: 15px;
+		padding: 1.5rem;
 	}
 	.ProblemHead {
 		display: flex;
@@ -263,22 +253,17 @@
 		border: 1px solid var(--outline);
 		border-radius: 5px;
 		padding: 5px;
-		width: 50%;
+		width: 60%;
 		height: 2.5rem;
 		font-size: 1rem;
-		line-height: 1.5rem;
+		line-height: 1.75rem;
 		resize: none;
 	}
 	.UserAndExDetails {
 		display: flex;
 		flex-direction: column;
-		width: 90%;
+		width: 100%;
 		gap: 12px;
-	}
-	.UsernameText {
-		font-size: 1rem;
-		line-height: 1.5rem;
-		font-weight: 300;
 	}
 	.ProblemDetailsInput {
 		border: 1px solid var(--outline);
@@ -287,21 +272,36 @@
 		width: 100%;
 		height: 50vh;
 		font-size: 1rem;
-		line-height: 1.5rem;
+		line-height: 1.75rem;
 		resize: none;
+	}
+	:global(.DoneButton){
+		align-self: self-end;
+		margin-right: 10%;
 	}
 	.CodeInputandOutput {
 		width: 100%;
 		height: calc(100vh - 5rem);
 		display: flex;
-		flex-direction: row;
+		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-		gap: 2.5%;
+		gap: 1rem;
 		scroll-snap-align: start;
 	}
+	.CodeInputandOutputWrap{
+		width: 90%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 2.5%;
+		background-color: var(--bg-50);
+		border: 1px solid var(--outline);
+		border-radius: 15px;
+		padding: 1.5rem;
+	}
 	.CodeInputBox {
-		width: 55%;
+		width: 70%;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
@@ -314,13 +314,13 @@
 		justify-content: space-between;
 	}
 	.CodeInput {
-		height: 75vh;
+		height: 65vh;
 		width: 100%;
 		border: 1px solid var(--outline);
 		border-radius: 5px;
 		padding: 5px;
 		font-size: 1rem;
-		line-height: 1.5rem;
+		line-height: 1.75rem;
 		resize: none;
 		white-space: nowrap;
 		overflow: auto;
@@ -348,27 +348,36 @@
 		gap: 12px;
 	}
 	.Output {
-		height: 75vh;
+		height: 65vh;
 		width: 100%;
 		border: 1px solid var(--outline);
 		border-radius: 5px;
 		padding: 5px;
 		font-size: 1rem;
-		line-height: 1.5rem;
+		line-height: 1.75rem;
 		resize: none;
 		white-space: nowrap;
 		overflow: auto;
 	}
 	.TestCase {
-		max-width: 90%;
+		width: 100%;
 		height: calc(100vh - 5rem);
+		gap: 1rem;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		justify-self: center;
 		justify-content: center;
-		gap: 16px;
 		scroll-snap-align: start;
+	}
+	.TestCaseBoxWrap{
+		max-width: 90%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		background-color: var(--bg-50);
+		border: 1px solid var(--outline);
+		border-radius: 15px;
 	}
 	.TestCaseBox {
 		display: flex;
@@ -377,12 +386,13 @@
 		overflow-x: auto;
 		white-space: nowrap;
 		scroll-behavior: smooth;
-		max-width: 100%;
-		padding-bottom: 15px;
+		max-width: 100%;	
+		padding-bottom: 1rem;
+		padding: 1.5rem;
+		scroll-padding-right: 2rem;
 		scroll-snap-type: x mandatory;
 	}
 	.TestCaseBox::-webkit-scrollbar {
-		width: 1vh;
 		height: 1vh;
 	}
 	.TestCaseBox::-webkit-scrollbar-thumb {
@@ -417,7 +427,7 @@
 		gap: 6px;
 		color: var(--text);
 		font-size: 1rem;
-		line-height: 1.5rem;
+		line-height: 1.75rem;
 	}
 	.CheckBox {
 		width: 1.25rem;
@@ -437,7 +447,7 @@
 		font-size: 1rem;
 	}
 	.TestCaseInput {
-		height: 35vh;
+		height: 32.5vh;
 		width: 100%;
 		border: 1px solid var(--outline);
 		border-radius: 5px;
@@ -462,7 +472,7 @@
 		box-shadow: none;
 	}
 	.TestCaseOutput {
-		height: 25vh;
+		height: 22.5vh;
 		width: 100%;
 		border: 1px solid var(--outline);
 		border-radius: 5px;
@@ -486,6 +496,23 @@
 	.TestCaseOutput:focus {
 		box-shadow: none;
 	}
+	:global(.AddTestCase){
+		position: relative;
+		top: 16px;
+		height: 56.5vh;
+		min-width: 7.5rem;
+		max-width: 7.5rem;
+		scroll-snap-align: end;
+	}
+	.ButtonWrap{
+		display: flex;
+		width: 100%;
+		align-items: center;
+		justify-content: space-between;
+	}
+	:global(.RunAll){
+		margin-left: 10%;
+	}
 	@media (max-width: 1000px) {
 		.CodeInputandOutput {
 			flex-direction: column;
@@ -500,10 +527,6 @@
 		.TestCaseContainer {
 			min-width: 15.5rem;
 			max-width: 15.5rem;
-		}
-		.AddTestCase {
-			min-width: 3.5rem;
-			max-width: 3.5rem;
 		}
 	}
 
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,7 +2,6 @@
 	import { goto } from "$app/navigation";
 	import { setCookie } from "$lib/cookie";
 	import * as api from "$lib/fetchApi";
-	import { onMount } from "svelte";
 	import IoIosEye from "svelte-icons/io/IoIosEye.svelte";
 	import IoIosEyeOff from "svelte-icons/io/IoIosEyeOff.svelte";
 	import "../../app.css";
@@ -97,7 +96,6 @@
 		border: 1px solid var(--theme-dark-text);
 		border-radius: 15px;
 		background-color: var(--bg-50);
-		opacity: 50;
 	}
 
 	.LoginHead {
