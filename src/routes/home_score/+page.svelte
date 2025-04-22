1446eb00b81fcd7bf93e80a57df15d56bc3b999e
Deaw-Veerapat <Veerapat231612@gmail.com>
2025-04-23 01:43:26 +0700

    Update +page.svelte

--- src/routes/home_score/+page.svelte
+++ src/routes/home_score/+page.svelte
@@ -1,3 +1,4 @@
+
 <script>
    import './HomeScore.css';
    import Score from '../../components/Score.svelte';
@@ -5,39 +6,21 @@
    $: query = $page.url.searchParams.get('page');
    export let data
    const { users }= data
-
+   import Fullscreen from '../../components/Fullscreen.svelte';
 </script>
+<Fullscreen>
 
-<div class="MenuBar">
-   <div class="left-section">
-      <img src="pictures/fu7.jpg" alt="Icon" class="circular-icon">
-      <span class="ce-boostup">CE-BOOSTUP</span>
+
+
+   <div class="OptionsMenu">
+      <nav class="menu">
+         <ul>
+            <li><a href="?page=overall">Overall</a></li>
+            <li><a href="?page=house_score">House</a></li>
+            <li><a href="?page=my_house_score">My House</a></li>
+         </ul>
+      </nav>
    </div>
-   <nav class="menuleft">
-      <ul>
-         <li><a href="?page=home">Home</a></li>
-         <li><a href="?page=about">About</a></li>
-         <li><a href="?page=services">Services</a></li>
-         <li><a href="?page=contact">Contact</a></li>
-      </ul>
-   </nav>
-   <button class="login-button">Login</button>
-</div>
-
-<div class="Leaderboard">
-   <h1>Leaderboard</h1>
-   <p>Track your progress and complete for the top!</p>
-</div>
-
-<div class="OptionsMenu">
-   <nav class="menu">
-      <ul>
-         <li><a href="?page=overall">Overall</a></li>
-         <li><a href="?page=house_score">House</a></li>
-         <li><a href="?page=my_house_score">My House</a></li>
-      </ul>
-   </nav>
-</div>
 {#if query == 'overall'}
 <div style="display: flex;justify-content:center; margin-top: 20px; ">
    <div style="width:80%; border:1px solid #767676; border-radius :20px; padding:20px; box-sizing: border-box; overflow:scroll; height:420px; " >
@@ -49,5 +32,5 @@
 </div>
 </div>
 {/if}
-
+</Fullscreen>
 
