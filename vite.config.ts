019da4322b7a0f7d03ee975035bab9a70da646d7
AzPepoze <pladumguy@gmail.com>
2025-05-03 17:09:15 +0700

    Removes monaco editor plugin
    Removes the vite plugin for the monaco editor as it is no longer needed.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 export default defineConfig(({ mode }) => {
 	return {
-		plugins: [
-			sveltekit(),
-			(monacoEditorPlugin as any).default({
-				languageWorkers: ["json"],
-			}),
-		],
+		plugins: [sveltekit()],
 		server: {
 			port: 3001,
 		},
