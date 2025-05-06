97aefac129aae90e18735e656e0ee8e073c2aedc
Kunmae ขุน <supakhon2549@gmail.com>
2025-05-06 22:22:35 +0700

    HomeScores_Fix_2

--- src/routes/home_score/+page.svelte
+++ src/routes/home_score/+page.svelte
@@ -4,18 +4,12 @@
      Imports
      -------------------------------------------------------
      */
-    // import './HomeScore.css';
     import Fullscreen from "../../components/Fullscreen.svelte";
     import Button from "../../components/Button.svelte";
-    // import { page } from '$app/stores';
-    // import { goto } from "$app/navigation";
+    import Frame from "../../components/Frame.svelte";
     import BadgeGold from "../../components/Icons/Badge_Gold.svelte";
     import BadgeSilver from "../../components/Icons/Badge_Silver.svelte";
     import BadgeBronze from "../../components/Icons/Badge_Bronze.svelte";
-    // import Frame from "../../components/Frame.svelte";
-    // $: query = $page.url.searchParams.get('page') || 'overall';
-    export let data;
-    const { users, houseScores, myHouseMembers, myHouseName } = data;
 
     const profile = {
       name: 'เพ็ญพิชชา ปานจันทร์',
@@ -30,85 +24,208 @@
 </script>
 
 <Fullscreen>
-    <!-- Left Card -->
-   <div class="profile-section">
-        <img src={profile.cardImg} alt="" class="profile-image">
-            <div class="profile-info">
-                <div class="profile-name-id-box">
-                    <span>{profile.name}</span>
-                    <span class="profile-id">{profile.studentId}</span>
-                </div>
-                <div class="profile-detail-top">นักผจญภัยอันดับที่ {profile.rank}</div> 
-                <div class="profile-detail-bottom">{profile.score}</div>
-                <div class="profile-detail-top">บ้านอันดับที่ {profile.houseRank}</div> 
-                <div class="profile-detail-bottom">{profile.houseScore}</div>
-                <Button class="profile-button">ประวัติคะแนน</Button>
+    <div id="Score">
+        <!-- SC-Left Side -->
+        <Frame id="sc-left" blur-bg>
+            <div style="display: flex; justify-content: center;">
+                <img src={profile.cardImg} alt="" id="scl-image">
             </div>
+            <div id="scl-main">
+                <div class="scl-top">
+                    <span>{profile.name}</span>
+                    <span style="color: var(--sc-text)">{profile.studentId} </span>
+                </div>
+                <Frame id="scl-detail-top">นักผจญภัยอันดับที่ {profile.rank}</Frame>
+                <div id="scl-detail-bottom">{profile.score}</div>
+                <Frame id="scl-detail-top">บ้านอันดับที่ {profile.houseRank}</Frame>
+                <div id="scl-detail-bottom">{profile.houseScore}</div>
+                <Button class="scl-btn">ประวัติคะแนน</Button>
+            </div>
+        </Frame>
+        <!-- SC-Right Side -->
+        <Frame id="sc-right" full="" blur-bg>
+            <div class="scr-top">
+                <Button class="scr-btn-Overall">Overall</Button>
+                <Button class="scr-btn-House">House</Button>
+                <Button class="scr-btn-MyHouse">MyHouse</Button>
+                
+            </div>
+            <div class="scr-bottom">
+                
+            </div>
+        </Frame>
     </div>
 
-    <div>
-        sdasdasdasdas
-    </div>
 </Fullscreen>
 
 <style lang="scss">
-    :global(*) {
-        transition: all 0.2s ease-out;
+    // -------------------------------------------------------
+    // Left Side SC GUI
+    // -------------------------------------------------------
+    #Score {
+        width: 100%;
+		height: 100%;
+        display: flex;
+		flex-direction: row;
+        gap: 0%;
+		padding: 3%;
+		box-sizing: border-box;
+		container-type: size;
+
+        :global(#sc-left) {
+            border-radius: 3%;
+            box-shadow: 0 4px 24px var(--list-shadow);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 38%;
+        }
+
+        #scl-image { width: 50%;}
+
+        #scl-main { 
+            display: flex; 
+            flex-direction: column;
+            background-color: var(--bg); 
+            border: 1px solid var(--outline);
+            border-radius: 10px;
+            padding: 3% 4%;
+            width: 87%;
+            height: 100%;
+        }
+
+        .scl-top { display: flex; justify-content: space-between; }
+
+        :global(#scl-detail-top) {
+            background-color: var(--sc-bg);
+            border: 1px solid var(--outline);
+            border-radius: 10px 10px 0 0;
+            margin: 5% 0 0 0;
+            padding: 2% 4%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        #scl-detail-bottom {
+            background-color: var(--bg);
+            border: 1px solid var(--outline);
+            border-top-width: 0;
+            border-radius: 0px 0px 10px 10px;
+            padding: 2% 4%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        :global(Button.scl-btn) {
+            background-color: var(--sc-bg);
+            border: 1px solid var(--outline);
+            padding: 3%;
+            margin: 6% 0 0 0;
+            box-shadow: 0 3px 5px var(--list-shadow);
+        }
+
+    // -------------------------------------------------------
+    // Right Side SC GUI
+    // -------------------------------------------------------
+        :global(#sc-right) { 
+            box-shadow: 0 4px 24px var(--list-shadow); 
+            padding: 1% 2%; 
+            width: 60%;
+        }
+
+        .scr-top { 
+            display: flex; 
+            border-radius: 20px;
+            border: 1px solid var(--outline);
+        }
+
+        :global(Button.scr-btn-Overall) { border-radius: 20px 0 0 20px; }
+        :global(Button.scr-btn-House) { border-radius: 0%;}
+        :global(Button.scr-btn-MyHouse) { border-radius: 0 20px 20px 0; }
+    
     }
+    // #sc-left {
+    //     background: var(--bg-50);
+    //     border-radius: 3%;
+    //     box-shadow: 0 4px 24px var(--list-shadow);
+    //     padding: 1.4% 1.4%;
+    //     margin: 2.25% 0 3% 6%;
+    //     display: flex;
+    //     flex-direction: column;
+    //     align-items: center;
+    //     width: 30%;
+    //     height: 91%;
+    // }
+    
+    // .profile-info {
+    //     background: var(--bg);
+    //     border: 1px solid var(--outline);
+    //     border-radius: 3%;
+    //     padding: 5% 5%;
+    //     margin: 0 0.6% 0.9% 0.6%;
+    //     width: 100%;
+    //     height: 50.5%;
+    // }
+
+    // #scl-detail-top {
+    //     background-color: var(--sc-bg);
+    //     border: 1px solid var(--outline);
+    //     border-radius: 10px 10px 0 0;
+    //     margin: 5% 0 0 0;
+    //     padding: 2% 4%;
+    //     display: flex;
+    //     flex-direction: column;
+    //     align-items: center;
+    // }
+
+    // #scl-detail-bottom {
+    //     background-color: var(--bg);
+    //     border: 1px solid var(--outline);
+    //     border-top-width: 0;
+    //     border-radius: 0px 0px 10px 10px;
+    //     padding: 2% 4%;
+    //     display: flex;
+    //     flex-direction: column;
+    //     align-items: center;
+    // }
+        
+    // @media (min-width: 800px) {
+    //     #profile-section {
+    //         :global(.profile-id) {
+
+    //         }
+    //     }
+    // }
+
     // -------------------------------------------------------
     // Left Side Score GUI
     // -------------------------------------------------------
-    .profile-section {
-        background: var(--bg-50);
-        border-radius: 24px;
-        box-shadow: 0 4px 24px var(--list-shadow, rgba(0,0,0,0.08));
-        padding: 30px 30px;
-        margin: 64px 0px 64px 110px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        width: 26%;
-        height: 87%;
-    }
-    .profile-image {
-        width: 350px;
-        height: auto;
-    }
-    .profile-info {
-        background: var(--bg);
-        border: 1px solid var(--outline);
-        border-radius: 10px;
-        padding: 18px 18px;
-        margin: 0px 15px 20px 15px;
-        width: 100%;
-    }
-    .profile-detail-top {
-        background-color: #F1D5A1;
-        border: 1px solid var(--outline);
-        border-radius: 10px 10px 0px 0px;
-        margin: 25px 0px 0px 0px;
-        padding: 5px 10px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    .profile-detail-bottom {
-        border: 1px solid var(--outline);
-        border-top-width: 0;
-        border-radius: 0px 0px 10px 10px;
-        padding: 5px 10px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    .profile-name-id-box {
-        display: flex;
-        justify-content: space-between;
-    }
-    .profile-id {
-        color: #A9844D;
-    }
-    :global(.profile-button) {
-        border: 1px solid #000000;
-    }
+        // #sc-right {
+        //     width: 100%;
+		//     height: 100%;
+        //     display: flex;
+        //     flex-direction: row;
+        //     padding: 15px;
+        //     background: var(--bg-50);
+        //     border-radius: 3%;
+        //     box-shadow: 0 4px 24px var(--list-shadow);
+        // }
+
+// <!-- <div id="sc-left">
+//         <img src={profile.cardImg} alt="" id="profile-image">
+//         <div class="profile-info">
+//             <div id="profile-name-id-box">
+//                 <span>{profile.name}</span>
+//                 <span style="color: var(--sc-text)">{profile.studentId}</span>
+//             </div>
+//             <div id="profile-detail-top">นักผจญภัยอันดับที่ {profile.rank}</div> 
+//             <div id="profile-detail-bottom">{profile.score}</div>
+//             <div id="profile-detail-top">บ้านอันดับที่ {profile.houseRank}</div> 
+//             <div id="profile-detail-bottom">{profile.houseScore}</div>
+//             <Button class="profile-button">ประวัติคะแนน</Button>
+//         </div>
+//     </div> -->
+
 </style>
