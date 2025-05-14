ed5c285b3fc1a0c27644d6dccf342b59da50ee2c
Archer-SN <superkakanoob@gmail.com>
2025-05-14 17:45:17 +0700

    Working on Toggling dark mode

---
+++ src/routes/menu/setting/Setting.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { theme } from "./shared.svelte";
+  function addDarkParam() {
+    const url = new URL(window.location.href);
+    theme.isDark = !theme.isDark;
+    window.history.pushState({}, "", `${url.toString()}&dark=${theme.isDark}`); // update URL without reloading
+  }
+</script>
+
+<div class="container">
+  <button on:click={addDarkParam}>Enable Dark Mode</button>
+</div>
+
+<style lang="scss">
+  .container {
+    width: 100%;
+    height: 100%;
+  }
+</style>
