ece3444cdf2751b4e08c99132528bad4727a40b4
Kunmae ขุน <supakhon2549@gmail.com>
2025-05-14 15:30:30 +0700

    Fix MyHouse Call data - Score

--- src/routes/menu/score/components/MyHouse.svelte
+++ src/routes/menu/score/components/MyHouse.svelte
@@ -3,35 +3,21 @@
     import * as api from "$lib/fetchApi";
     import { onMount } from "svelte";
 
-    // MyHouse - get ข้อมูลของคนในบ้าน /house/ชื่อบ้าน
-	// let houseScore = api.call(`/house/barbarian`);
-
     let dataMyHouse: any[] = [];
     let optionDropdown: any[] = [];
     let selectedOptionDropdown: string = "Barbarian";
-    // const optionDropdown: { value: string; }[] = [
-    // { value: 'Barbarian' }, { value: 'Rogue' }, { value: 'Sorcerer' },{ value: 'Bard' }, 
-    // { value: 'Monk' }, { value: 'Paladin' }, { value: 'Wizard' }, { value: 'Priest' }, 
-    // { value: 'Ranger' }, { value: 'Warlock' }, { value: 'Samurai' },
-    // ];
-
-    // let dataMyHouse = [
-    // { name: "Veerapat Pirultham", studnetId: "67010852", score: 720 },
-    // { name: "Nattapong Suksiri", studnetId: "67010853", score: 700 },
-    // { name: "Kamonchai Lekbun", studnetId: "67010854", score: 680 },
-    // { name: "Somsak Pradchaphet", studnetId: "67010855", score: 650 },
-    // { name: "Patsorn Chaiyawan", studnetId: "67010856", score: 640 },
-    // ];
 
     onMount(async () => {
         const house = await api.call(`/houseScores?order=ASC`);
         optionDropdown = house.data;
-
-        const myHouse = await api.call(`/house/${selectedOptionDropdown.toLowerCase()}`)
-        dataMyHouse = myHouse.users;
-
     });
 
+    $: if (selectedOptionDropdown) {
+        (async() => {
+            const myHouse = await api.call(`/house/${selectedOptionDropdown.toLowerCase()}`)
+            dataMyHouse = myHouse.users;
+        })();
+    }
 </script>
 
 <div class="myHouseLeaderboard">
@@ -40,7 +26,6 @@
             <option>{option.name.charAt(0).toUpperCase() + option.name.slice(1)}</option>
         {/each}
     </select>
-    <!-- <p>{selectedOptionDropdown.toLowerCase()}</p> -->
 </div>
 
 {#each dataMyHouse as user, i}
